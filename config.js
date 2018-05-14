50fb104ce1125dc221a9eb3ca66fb4afe2470665
Guglielmo Pepe <1424836+GuglielmoPepe@users.noreply.github.com>
2018-05-14 17:10:35 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+angular.module('portfolio.config',[])
+  .constant('USER_URL', 'https://api.github.com/users/GuglielmoPepe')
+  .constant('REPOS_URL', 'https://api.github.com/users/GuglielmoPepe/repos')
+;  
